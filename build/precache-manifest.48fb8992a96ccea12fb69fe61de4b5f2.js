9de50c840f9bff8d7bb7db44755c628e8f6b9464
JoshAubrey <joshaubrey7@gmail.com>
2020-09-28 09:29:12 -0500

    uibuild

---
+++ build/precache-manifest.48fb8992a96ccea12fb69fe61de4b5f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b1c7012e57f1f33cdd7cd1f25583ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d3a7a18b769f6d87110",
+    "url": "/static/css/main.878c1d4c.chunk.css"
+  },
+  {
+    "revision": "5025582f0bba5a0d1c75",
+    "url": "/static/js/2.d0ffda05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0ffda05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d3a7a18b769f6d87110",
+    "url": "/static/js/main.ba244d19.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('reqBody', function getReqBody (req) {
     const body = req.body
